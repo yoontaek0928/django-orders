98e1f7fcc3d8dceb3204d80695e83db071a40d47
eeyoontaek <eeyoontaek@gmail.com>
2022-06-23 10:45:48 +0900

    FINISH orders

--- djangoOrders/djangoOrders/settings.py
+++ djangoOrders/djangoOrders/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import pymysql
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -27,6 +28,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+pymysql.install_as_MySQLdb()
 
 # Application definition
 
@@ -79,10 +81,20 @@ WSGI_APPLICATION = 'djangoOrders.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'project1',
+        'USER':'admin',
+        'PASSWORD':'512mdkmdk!',
+        'HOST':'database-1.cp0xkjubgosy.us-east-2.rds.amazonaws.com',
+        'PORT':'3306',
+        'OPTIONS':{
+            'init_command' : "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
+
+# 데이터베이스 이름 : project1
+# 비밀번호 : 512mdkmdk!
 
 
 # Password validation
