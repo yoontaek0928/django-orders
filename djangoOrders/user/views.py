35a257e47b506dce02cbaf4a643ded42ef7327b0
eeyoontaek <eeyoontaek@gmail.com>
2022-06-24 14:26:01 +0900

    ADD user views

--- djangoOrders/user/views.py
+++ djangoOrders/user/views.py
@@ -6,7 +6,16 @@ from django.views.decorators.csrf import csrf_exempt
 from rest_framework.parsers import JSONParser
 from django.utils import timezone
 
-# @csrf_exempt
-# def user(request):
-#     if request.method == 'GET':
-#         user = User.objects.all()
+@csrf_exempt
+def user(request):
+    if request.method == 'GET':
+        user = User.objects.all()
+        return render(request, 'user/user_list.html',{'user_list':user})
+    
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = UserSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
